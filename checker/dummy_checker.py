f4326e69f189ab8ac160758b5967a041205dbfec
Sanidhya Kafle <110410335+sanidhya253@users.noreply.github.com>
2025-12-03 01:00:19 +0545

    Modify dummy_checker to simulate a failure
    Updated dummy_checker.py to force a failure for demo purposes.

--- checker/dummy_checker.py
+++ checker/dummy_checker.py
@@ -1,2 +1,6 @@
-print("CI/CD pipeline working!")
-exit(0)  # success
+# checker/dummy_checker.py
+print("CI/CD pipeline working — but now we force a failure for demo")
+# exit 1 means failure
+import sys
+sys.exit(1)
+
